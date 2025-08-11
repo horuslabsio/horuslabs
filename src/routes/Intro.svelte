449415e80a125f800b90df0d6acc53502a1652f7
Horus Labs <horuslabsio@gmail.com>
2025-08-11 18:57:57 +0100

    Update Intro.svelte

--- src/routes/Intro.svelte
+++ src/routes/Intro.svelte
@@ -2,7 +2,7 @@
 	<h2>Who we are</h2>
 
 	<p>
-		Horus Labs is a research and development lab on a mission to build world-class products from within Africa. 
+		Horus Labs is a startup studio on a mission to build world-class products from within Africa. 
 		Founded in April, 2024, we believe that great builders can come from anywhere. 
 		We seek out ambitious talents with the hunger to grow and the drive to thrive, giving them the tools and opportunities needed to build the future.
 	</p>
